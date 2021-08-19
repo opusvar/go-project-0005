0573eda4205ba666fbe84cae689e1ac634ecca7b
opusvar <sprojdev@gmail.com>
2021-08-19 08:44:57 -0400

    Added error handling to greetings.go

---
+++ greetings/greetings.go
@@ -0,0 +1,15 @@
+package greetings
+
+import "fmt"
+
+// return a greeting with an emeded name in in a message
+
+func Hello(name string) (string, error) {
+    if name == ""	 {
+		return "", errors.New("empty name")
+	}
+
+
+	message := fmt.Sprintf("Hi %v. Welcome!", name)
+	return message, nil
+}
